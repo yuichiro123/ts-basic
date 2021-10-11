380368c648cba32696fba0824ae8fa9430b4b663
yuichiro <yuihciroyamamoto1982@gmail.com>
2021-10-11 10:47:01 +0900

    First commit

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import World from './world'
+
+const root = document.getElementById('root')
+const world = new World('hello yamamoto!')
+world.sayHello(root)
---
+++ src/world.ts
@@ -0,0 +1,17 @@
+export default class World {
+  message: string
+
+  constructor(message: string) {
+    this.message = message
+  }
+
+  public sayHello(elem: HTMLElement | null): void {
+    if (elem) {
+      elem.innerText = this.message
+
+
+
+      
+    }
+  }
+}
